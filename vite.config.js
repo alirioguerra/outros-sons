1f926453a9fb156b699fd77f2a744addc724bca4
Alírio Guerra <alirioguerra.92@gmail.com>
2023-07-26 09:21:44 -0300

    chore(project): clear & add dev dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+              @import "@/assets/css/style.scss";
+            `
+      }
+    }
+  }
 })
