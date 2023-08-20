aef5cec37972199cabd832a20bc2a02eb4767178
Alírio Guerra <alirio@follow55.com.br>
2023-08-20 12:22:56 -0300

    chore(app): add real related links

--- src/App.vue
+++ src/App.vue
@@ -22,18 +22,23 @@ const videos = [
     relatedLinks: [
       {
         id: 1,
-        url: 'https://www.youtube.com/watch?v=Z2Q7Xh6ZJMc',
-        title: 'Exposição de Cecília Lima e Iracema Barbosa'
+        url: 'https://www.cultura.df.gov.br/fac-fundodeapoioacultura/',
+        title: 'FAC'
       },
       {
         id: 2,
-        url: 'https://www.youtube.com/watch?v=Z2Q7Xh6ZJMc',
-        title: 'Exposição de Cecília Lima e Iracema Barbosa'
+        url: 'http://museu.cultura.df.gov.br/',
+        title: 'Museu Nacional'
       },
       {
         id: 3,
-        url: 'https://www.youtube.com/watch?v=Z2Q7Xh6ZJMc',
-        title: 'Exposição de Cecília Lima e Iracema Barbosa'
+        url: 'https://dasartes.com.br/agenda/iracema-barbosa-textura-dos-afetos-museu-nacional-da-republica/',
+        title: 'Trabalhos de Iracema Barbosa'
+      },
+      {
+        id: 4,
+        url: 'https://biacruzz2002.wixsite.com/bia-cruz?fbclid=PAAaaFMWuKHsF6nAzrPKVG1b-8IEFT8z_DPFNArv6hcaWJRuG7ENDaocLS-9Y',
+        title: 'Artigos da Beatriz Cruz'
       }
     ]
   },
@@ -47,18 +52,43 @@ const videos = [
     relatedLinks: [
       {
         id: 1,
-        url: 'https://www.youtube.com/watch?v=Z2Q7Xh6ZJMc',
-        title: 'Exposição de Cecília Lima e Iracema Barbosa'
+        url: 'http://www.planalto.gov.br/ccivil_03/leis/2002/L10436.htm',
+        title: 'Lei de acessibilidade em Libras'
+      },
+      {
+        id: 33,
+        url: 'https://www.gov.br/mec/pt-br/assuntos/noticias/2023/abril/mec-promove-seminario-sobre-21-anos-da-lei-de-libras',
+        title: 'Lei de acessibilidade em Libras - 2'
       },
       {
         id: 2,
-        url: 'https://www.youtube.com/watch?v=Z2Q7Xh6ZJMc',
-        title: 'Exposição de Cecília Lima e Iracema Barbosa'
+        url: 'https://www.educacao.df.gov.br/concursos-da-seedf/',
+        title: 'Concursos SEEDF'
       },
       {
         id: 3,
-        url: 'https://www.youtube.com/watch?v=Z2Q7Xh6ZJMc',
-        title: 'Exposição de Cecília Lima e Iracema Barbosa'
+        url: 'https://www.ifsc.edu.br/postagens-blog-intercambistas/-/asset_publisher/qYC5Mt2Bw6wv/content/id/2336635/como-funciona-o-trabalho-de-um-tradutor-int%C3%A9rprete-de-libras',
+        title: 'Intérprete de Libras'
+      },
+      {
+        id: 4,
+        url: 'https://www.unb.br/',
+        title: 'UNB'
+      },
+      {
+        id: 5,
+        url: ' https://www.instagram.com/ebtaguatinga/',
+        title: 'Escola Bilíngue de Taguatinga-DF'
+      },
+      {
+        id: 6,
+        url: ' https://www.sinprodf.org.br/ ',
+        title: 'O que é o SINPRO'
+      },
+      {
+        id: 7,
+        url: 'https://www.scielo.br/j/eh/a/SGqJ6b5C4m44vh8R5hPV78m/?lang=pt ',
+        title: 'Desafios da educação na pandemia'
       }
     ]
   },
@@ -72,18 +102,18 @@ const videos = [
     relatedLinks: [
       {
         id: 1,
-        url: 'https://www.youtube.com/watch?v=Z2Q7Xh6ZJMc',
-        title: 'Exposição de Cecília Lima e Iracema Barbosa'
+        url: 'https://ibadeb.com.br/formacoes/presencial/libras#:~:text=O%20Instituto%20B%C3%ADblico%20da%20Assembleia,Sinais%20e%20Garantir%20uma%20comunica%C3%A7%C3%A3o',
+        title: 'Onde fazer cursos de Libras no DF'
       },
       {
         id: 2,
-        url: 'https://www.youtube.com/watch?v=Z2Q7Xh6ZJMc',
-        title: 'Exposição de Cecília Lima e Iracema Barbosa'
+        url: 'https://academiadelibras.com/blog/lingua-de-sinais-americana/',
+        title: 'Asl EUA'
       },
       {
         id: 3,
-        url: 'https://www.youtube.com/watch?v=Z2Q7Xh6ZJMc',
-        title: 'Exposição de Cecília Lima e Iracema Barbosa'
+        url: 'https://ictdf.org.br/',
+        title: 'ICTDF'
       }
     ]
   },
@@ -97,18 +127,28 @@ const videos = [
     relatedLinks: [
       {
         id: 1,
-        url: 'https://www.youtube.com/watch?v=Z2Q7Xh6ZJMc',
-        title: 'Exposição de Cecília Lima e Iracema Barbosa'
+        url: 'https://fbdsdf.org.br/ ',
+        title: 'FBDS'
       },
       {
         id: 2,
-        url: 'https://www.youtube.com/watch?v=Z2Q7Xh6ZJMc',
-        title: 'Exposição de Cecília Lima e Iracema Barbosa'
+        url: 'https://www.gov.br/pt-br/noticias/cultura-artes-historia-e-esportes/2022/05/brasil-fecha-surdolimpiadas-com-o-maior-numero-de-medalhas-da-historia ',
+        title: 'Surdolimpíadas'
       },
       {
         id: 3,
-        url: 'https://www.youtube.com/watch?v=Z2Q7Xh6ZJMc',
-        title: 'Exposição de Cecília Lima e Iracema Barbosa'
+        url: 'https://editorarealize.com.br/editora/anais/conedu/2018/TRABALHO_EV117_MD4_SA10_ID7105_20082018113008.pdf ',
+        title: 'Benefícios do esporte para o surdo'
+      },
+      {
+        id: 4,
+        url: 'https://handebolminuto.wordpress.com/2017/02/28/handebol-de-surdos-no-brasil-conheca-um-pouco-da-historia/ ',
+        title: 'Seleção Brasileira de Handebol Surdo'
+      },
+      {
+        id: 5,
+        url: ' https://cpb.org.br/ ',
+        title: 'Jogos Paralímpicos'
       }
     ]
   }
